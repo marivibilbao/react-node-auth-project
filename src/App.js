d5146e306cf0ab248633615c8ee05fab7c41b98c
Maria Victoria <marivibilbaog@gmail.com>
2021-10-22 15:07:03 +0200

    Agrego botones

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
-import './App.css';
+import React from 'react';
+import "./App.css";
+
+// Ejecuté comando: npm install react-router-dom 
+// según lo leido en la web: https://johnserrano.co/blog/aprende-a-crear-rutas-con-react-router
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>Authentication React App</h1>
+        <div class="btn-group" role="group" aria-label="Basic mixed styles example">
+          <button type="button" class="btn btn-danger">Login</button>
+          <button type="button" class="btn btn-warning">Sign-up</button>
+          <button type="button" class="btn btn-success">Home</button>
+        </div>
       </header>
     </div>
   );
-}
+};
 
 export default App;
